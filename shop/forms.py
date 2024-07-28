b28897de223b7cea2c6fa325383f983bd96c52b2
sepideh <sepideh.hayati16@Gmail.com>
2024-07-28 09:05:18 +0330

    add users

---
+++ shop/forms.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' نام خود را وارد کنید'}),
+        label='',
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' نام خانوادگی خود را وارد کنید'}),
+        label='',
+    )
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' ایمیل خود را وارد کنید'}),
+        label='',
+    )
+    username = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' نام کاربری'}),
+        label='',
+    )
+    password1 = forms.CharField(
+        label='',
+        max_length=30,
+        widget= forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'name': 'password',
+                'type': 'password',
+                'placeholder': ' رمز بالای 8 کارکتر را وارد کنید'
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        label='',
+        max_length=30,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'name': 'password',
+                'type': 'password',
+                'placeholder': ' دوباره رمز خود را وارد کنید'
+            }
+        )
+    )
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import SignUpForm
 
 # Create your views here.
 def hello(request):
@@ -33,3 +37,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'با موفقیت خارج شدید')
     return redirect('home')
+
+def signup_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password1)
+            login(request, user)
+            messages.success(request,'اکانت شما ساخته شد')
+            return redirect('home')
+        else:
+            messages.success(request, 'مشکلی در ثبت نام شما وجود دارد')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html',{'form':form})
