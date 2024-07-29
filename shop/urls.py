95a88c37187a3effe4c9e74f33cf13add8f0b380
sepideh <sepideh.hayati16@Gmail.com>
2024-07-29 07:38:01 +0330

    add catrgory

--- shop/urls.py
+++ shop/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('signup/', views.signup_user, name='signup'),
     path('product/<int:pk>', views.product, name='product'),
+    path('category/<str:cat>', views.category, name='category'),
 ]
