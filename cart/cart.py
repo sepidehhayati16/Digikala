5836e02bb83bf9975cd7329d6e4d3f92c1fad5ec
sepideh <sepideh.hayati16@Gmail.com>
2024-07-30 15:17:08 +0330

    Total orders in the shopping cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,5 @@
 from shop.models import  Product
+from decimal import Decimal
 
 class Cart:
     def __init__(self,request):
@@ -31,6 +32,25 @@ class Cart:
     def get_quants(self):
         quantities = self.cart
         return quantities
+
+    def get_total(self):
+        product_ids = map(int, self.cart.keys())  # تبدیل کلیدها به اعداد صحیح
+        products = Product.objects.filter(id__in=product_ids)
+
+        total = Decimal(0)
+        product_dict = {product.id: product for product in products}  # تبدیل لیست محصولات به دیکشنری
+
+        for key, value in self.cart.items():
+            product = product_dict.get(int(key))
+            if product:
+                quantity = Decimal(value)
+                if product.is_sale:
+                    total += Decimal(product.sale_price) * quantity
+                else:
+                    total += Decimal(product.price) * quantity
+
+        return total
+
 
     def update(self,product,quantity):
         product_id = str(product)
