805c40985565c7e9a5b31cc55b22310a5a823237
sepideh <sepideh.hayati16@Gmail.com>
2024-07-30 08:34:38 +0330

    Add Cart page

--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart
+from shop.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart_summary(request):
-    return render(request,'cart_summary.html', {})
+    cart = Cart(request)
+    cart_products = cart.get_prods()
+    return render(request,'cart_summary.html', {'cart_products': cart_products})
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+
+        cart_quantity = cart.__len__()
+        # response = JsonResponse({'Product name': 'product.name'})
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
